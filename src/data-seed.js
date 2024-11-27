ae35b237699571637bda26861e2068cc2fa94017
Zevhys <rakha2728@gmail.com>
2024-11-27 15:01:00 +0800

    add data dummy product

---
+++ src/data-seed.js
@@ -0,0 +1,125 @@
+import productImage1 from "./assets/photo-1.webp";
+import productImage2 from "./assets/photo-2.webp";
+import productImage3 from "./assets/photo-3.webp";
+import productImage4 from "./assets/photo-4.webp";
+import productImage5 from "./assets/photo-5.webp";
+import productImage6 from "./assets/photo-6.webp";
+import productImage7 from "./assets/photo-7.webp";
+import productImage8 from "./assets/photo-8.webp";
+import productImage9 from "./assets/photo-9.webp";
+import productImage10 from "./assets/photo-10.webp";
+import productImage11 from "./assets/photo-11.webp";
+import productImage12 from "./assets/photo-12.webp";
+
+export const products = [
+  {
+    id: "123",
+    name: "Black Wallet",
+    price: "60.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage1,
+    averageRating: "5.0",
+  },
+  {
+    id: "234",
+    name: "iPhone Black",
+    price: "120.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage2,
+    averageRating: "4.9",
+  },
+  {
+    id: "345",
+    name: "Beer Can",
+    price: "90.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage3,
+    averageRating: "4.8",
+  },
+  {
+    id: "456",
+    name: "Powder Tap",
+    price: "190.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage4,
+    averageRating: "4.7",
+  },
+  {
+    id: "567",
+    name: "Aerosol",
+    price: "75.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage5,
+    averageRating: "4.6",
+  },
+  {
+    id: "678",
+    name: "Sun Glasses",
+    price: "200.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage6,
+    averageRating: "4.5",
+  },
+  {
+    id: "789",
+    name: "Dark Shoes",
+    price: "100.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage7,
+    averageRating: "4.4",
+  },
+  {
+    id: "890",
+    name: "Sport Shoes",
+    price: "40.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage8,
+    averageRating: "4.3",
+  },
+  {
+    id: "901",
+    name: "Lens",
+    price: "54.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage9,
+    averageRating: "4.2",
+  },
+  {
+    id: "109",
+    name: "Woman Shoes",
+    price: "330.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage10,
+    averageRating: "4.1",
+  },
+  {
+    id: "987",
+    name: "Camera",
+    price: "230.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage11,
+    averageRating: "4.0",
+  },
+  {
+    id: "908",
+    name: "White Phone",
+    price: "180.000",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt.",
+    imageUrl: productImage12,
+    averageRating: "3.9",
+  },
+];
+
+export const cartItems = [products[0], products[2], products[3]];
